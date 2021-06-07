8c0bca3376043b790193120e14557bfc40e50788
Nimesh <n.nischal.1994@gmail.com>
2021-06-07 10:29:50 +0530

    Init Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+    public GameObject completeLevelUI;
+    public PlayerMovement movement;
+
+    public void EndGame ()
+    {
+        if (!gameHasEnded)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over!");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void CompleteLevel()
+    {
+        Debug.Log("Level won!");
+        completeLevelUI.SetActive(true);
+        movement.enabled = false;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwardForce = 500f;
+    public float lateralForce = 50f;
+    public float jumpForce = 500f;
+
+    private bool moveLeft = false;
+    private bool moveRight = false;
+    private bool jump = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        moveLeft = Input.GetKey("a");
+        moveRight = Input.GetKey("d");
+        jump = Input.GetKeyDown("space");
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce);
+        // transform.Translate(Vector3.forward / (forwardForce /10));
+        if (moveLeft)
+            rb.AddForce(-lateralForce, 0, 0, ForceMode.VelocityChange);
+        if (moveRight)
+            rb.AddForce(lateralForce, 0, 0, ForceMode.VelocityChange);
+        if (jump)
+            rb.AddForce(0, jumpForce, 0);
+        if (rb.position.y < -1)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
